2a6bf616612964e89f93919a7a96394540313f44
roaringcow <99341198+RoaringCow@users.noreply.github.com>
2023-10-08 13:57:45 +0300

    Update sonify.sh

--- sonify.sh
+++ sonify.sh
@@ -11,7 +11,8 @@ ffmpeg -i $1 frames/thumb%06d.png -hide_banner
 # Build the Rust project
 cargo build --release
 
+# Run and measure the execution time of the program
+time ./target/release/rustsound
+
 # Combine the video and the sound.
 ffmpeg -i $1 -i target/release/ses.wav -map 0:v -map 1:a -c:v copy -shortest output.mp4
-# Measure the execution time of the program
-time ./target/release/rustsound
