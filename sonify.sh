44833e83753d4fe6f191dd74a1c211106cdc1909
roaringcow <99341198+RoaringCow@users.noreply.github.com>
2023-10-08 13:59:33 +0300

    Update sonify.sh

--- sonify.sh
+++ sonify.sh
@@ -15,4 +15,4 @@ cargo build --release
 time ./target/release/rustsound
 
 # Combine the video and the sound.
-ffmpeg -i $1 -i target/release/ses.wav -map 0:v -map 1:a -c:v copy -shortest output.mp4
+ffmpeg -i $1 -i ses.wav -map 0:v -map 1:a -c:v copy -shortest output.mp4
